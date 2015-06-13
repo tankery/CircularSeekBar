7470650543c363136d77f2dcf473018b9ee0a943
Tankery Chen <tankery.chen@gmail.com>
2015-06-13 10:49:51 +0800

    Init create Android Studio project.

---
+++ app/src/main/java/me/tankery/app/circularseekbar/MainActivity.java
@@ -0,0 +1,15 @@
+package me.tankery.app.circularseekbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
