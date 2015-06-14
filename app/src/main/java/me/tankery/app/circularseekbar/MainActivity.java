4ee116d75cd5b0671b12d8e044fc3e2f12e96e54
Tankery Chen <tankery.chen@gmail.com>
2015-06-14 10:20:41 +0800

    Add progress track.

--- app/src/main/java/me/tankery/app/circularseekbar/MainActivity.java
+++ app/src/main/java/me/tankery/app/circularseekbar/MainActivity.java
@@ -2,6 +2,9 @@ package me.tankery.app.circularseekbar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import me.tankery.lib.circularseekbar.CircularSeekBar;
 
 
 public class MainActivity extends Activity {
@@ -10,6 +13,24 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CircularSeekBar seekBar = (CircularSeekBar) findViewById(R.id.seek_bar);
+        seekBar.setOnSeekBarChangeListener(new CircularSeekBar.OnCircularSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(CircularSeekBar circularSeekBar, float progress, boolean fromUser) {
+                Log.d("Main", String.format("Progress changed to %.2f, fromUser %s", progress, fromUser));
+            }
+
+            @Override
+            public void onStopTrackingTouch(CircularSeekBar seekBar) {
+                Log.d("Main", "onStopTrackingTouch");
+            }
+
+            @Override
+            public void onStartTrackingTouch(CircularSeekBar seekBar) {
+                Log.d("Main", "onStartTrackingTouch");
+            }
+        });
     }
 
 }
